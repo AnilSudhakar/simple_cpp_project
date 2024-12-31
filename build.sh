3f8f7b97215b6c3ae6328b59abe6e10b02b82efc
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2024-12-31 20:30:47 +0100

    added conan install setp

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ CLEAR='\033[0m'
 
 BUILD_DIR="_build-output"
 
-# conan install . --output-folder=$BUILD_DIR --build=missing
-# if [ $? -ne 0 ]; then
-#     echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
-#     exit 1
-# fi
+conan install . --output-folder=$BUILD_DIR --build=missing
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
+    exit 1
+fi
 
 cmake -GNinja -B $BUILD_DIR
 
