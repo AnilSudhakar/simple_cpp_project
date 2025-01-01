3dd8e7c06170b6f1bc6cf5285f572ecfc2d2227f
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-01 22:48:18 +0100

    TEST COMMIT commented the conan install step

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ CLEAR='\033[0m'
 
 BUILD_DIR="_build-output"
 
-conan install . --output-folder=$BUILD_DIR --build=missing
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
-    exit 1
-fi
+# conan install . --output-folder=$BUILD_DIR --build=missing
+# if [ $? -ne 0 ]; then
+#     echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
+#     exit 1
+# fi
 
 cmake -GNinja -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
