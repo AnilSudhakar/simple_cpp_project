b9364a1d215e95f83af7e6729c7bdc32436a3267
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-02 12:54:33 +0100

    uncommented the code

--- build.sh
+++ build.sh
@@ -4,12 +4,13 @@ RED='\033[0;31m'
 CLEAR='\033[0m'
 
 BUILD_DIR="_build-output"
+echo "PATH: $PATH"
 
-# conan install . --output-folder=$BUILD_DIR --build=missing
-# if [ $? -ne 0 ]; then
-#     echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
-#     exit 1
-# fi
+conan install . --output-folder=$BUILD_DIR --build=missing
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
+    exit 1
+fi
 
 cmake -GNinja -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
