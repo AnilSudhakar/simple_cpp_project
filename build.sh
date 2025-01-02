ec5fe5be0e2aa08efd85aa56061faa78f1fdead3
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-02 17:15:10 +0100

    TEST COMMIT

--- build.sh
+++ build.sh
@@ -6,36 +6,30 @@ CLEAR='\033[0m'
 BUILD_DIR="_build-output"
 echo "PWD: $PWD"
 echo "PATH: $PATH"
-echo "============================cd============================"
-cd
-echo "============================ls============================"
-ls -all
-echo "============================cd home============================"
-cd /home
-echo "============================ls============================"
-ls -all
+echo "User ID: $(id -u)"
+echo "Group ID: $(id -g)"
 
-conan install . --output-folder=$BUILD_DIR --build=missing
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
-    exit 1
-fi
+# conan install . --output-folder=$BUILD_DIR --build=missing
+# if [ $? -ne 0 ]; then
+#     echo -e "${RED}Conan failed to install dependencies. Check logs for additional information${CLEAR}"
+#     exit 1
+# fi
 
-cmake -GNinja -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+# cmake -GNinja -B $BUILD_DIR -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/build/Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 
-if [ $? -ne 0 ]; then
-    echo -e "${RED}CMake failed to generate build system. Check logs for additional information${CLEAR}"
-    exit 1
-fi
+# if [ $? -ne 0 ]; then
+#     echo -e "${RED}CMake failed to generate build system. Check logs for additional information${CLEAR}"
+#     exit 1
+# fi
 
-cmake --build $BUILD_DIR
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Build failed. Check logs for additional information${CLEAR}"
-    exit 1
-fi
+# cmake --build $BUILD_DIR
+# if [ $? -ne 0 ]; then
+#     echo -e "${RED}Build failed. Check logs for additional information${CLEAR}"
+#     exit 1
+# fi
 
-cmake --install $BUILD_DIR --prefix $BUILD_DIR/install
-if [ $? -ne 0 ]; then
-    echo -e "${RED}Installation failed. Check logs for additional information${CLEAR}"
-    exit 1
-fi
+# cmake --install $BUILD_DIR --prefix $BUILD_DIR/install
+# if [ $? -ne 0 ]; then
+#     echo -e "${RED}Installation failed. Check logs for additional information${CLEAR}"
+#     exit 1
+# fi
