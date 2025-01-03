ad0b2a3ecfcd226aca459fdf518ff5e4d4d178bd
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-03 16:32:10 +0100

    removed redundant options

--- run_docker.sh
+++ run_docker.sh
@@ -1,37 +1,5 @@
 #!/bin/bash
 
-CI_BUILD="false"
-
-ARGUMENT_LIST=(
-  "ci-build"
-)
-
-OPTS=$(getopt \
-  --longoptions "$(printf "%s," "${ARGUMENT_LIST[@]}")" \
-  --name "$(basename "$0")" \
-  --options "" \
-  -- "$@"
-)
-if [ $? -ne 0 ]; then
-  echo "Failed to parse options...exiting."
-  exit 1
-fi
-
-eval set -- "$OPTS"
-
-while true; do
-  case "$1" in
-  --ci-build)
-    CI_BUILD="true"
-    shift
-    ;;
-  --)
-    shift
-    break
-    ;;
-  esac
-done
-
 DOCKER_IMAGE_NAME="anilkaiy.jfrog.io/docker-trial/development_docker"
 TAG_NAME="1"
 
@@ -55,14 +23,9 @@ if [ ! -d "$HOST_CONAN_DIR" ]; then
 fi
 
 DOCKER_RUN_ARGS=(
-  --user "$USER_ID:$GROUP_ID" --rm
+  --user "$USER_ID:$GROUP_ID" --rm -it
   -v "$ABSOLUTE_PATH:/home/docker"
   -v "/home/$USER/.conan:/home/ubuntu/.conan2"
 )
-
-if [ "$CI_BUILD" == "false" ]; then
-  DOCKER_RUN_ARGS+=(-it)
-  echo "Running in interactive mode..."
-fi
 
 docker run "${DOCKER_RUN_ARGS[@]}" $DOCKER_IMAGE_NAME:$TAG_NAME
