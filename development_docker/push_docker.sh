b29050683fd2bfecb6c2a5f7b77c3e6ade463a4a
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-03 16:29:14 +0100

    updated docker image

--- development_docker/push_docker.sh
+++ development_docker/push_docker.sh
@@ -2,5 +2,5 @@
 
 docker login -uanils.kaiyamballi@gmail.com anilkaiy.jfrog.io
 
-docker tag development_docker anilkaiy.jfrog.io/docker-trial/development_docker:latest
-docker push anilkaiy.jfrog.io/docker-trial/development_docker:latest
+docker tag development_docker anilkaiy.jfrog.io/docker-trial/development_docker:1
+docker push anilkaiy.jfrog.io/docker-trial/development_docker:1
