650bce5eebef155e8c22e75361498f25eb78d56b
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-03 14:18:58 +0100

    TEST COMMIT

--- run_docker.sh
+++ run_docker.sh
@@ -62,8 +62,10 @@ DOCKER_RUN_ARGS=(
 
 if [ "$CI_BUILD" == "false" ]; then
   DOCKER_RUN_ARGS+=(-it)
+  echo "Running in interactive mode..."
 else
   DOCKER_RUN_ARGS+=(-d)
+  echo "Running in detached mode..."
 fi
 
-docker run "${DOCKER_RUN_ARGS[@]}" development_docker /bin/bash -c "echo 'Hello from inside the container'"
+docker run "${DOCKER_RUN_ARGS[@]}" development_docker
