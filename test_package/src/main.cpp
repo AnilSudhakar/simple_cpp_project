5cd89a93045e402a9e092577a49ee1a9a3fcf24d
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-05 19:42:40 +0100

    added log level for every log

--- test_package/src/main.cpp
+++ test_package/src/main.cpp
@@ -2,10 +2,12 @@
 
 int main() {
     logging::Logger& logger = logging::Logger::getInstance();
-    logger.setLogLevel(logging::LogLevel::DEBUG);
+    logger.setLogLevel(logging::LogLevel::INFO);
     logger.setOutputFile("log.txt");
     logger.log(logging::LogLevel::INFO, "Hello, World!");
+    logger.setLogLevel(logging::LogLevel::DEBUG);
     logger.log(logging::LogLevel::DEBUG, "This is a debug message.");
+    logger.setLogLevel(logging::LogLevel::ERROR);
     logger.log(logging::LogLevel::ERROR, "This is an error message.");
     return 0;
 }
