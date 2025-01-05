65d91fcb5addc11e8b91e0428e51c20b60dbf1e5
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-05 18:45:45 +0100

    added package to upload simple_cpp_project

---
+++ upload_package.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+
+echo "${GREEN}Uploading Conan package...${CLEAR}"
+
+conan remote add simple-conan https://anilkaiy.jfrog.io/artifactory/api/conan/simple-conan
+conan upload simple_cpp_project/0.0.1@official/release --remote=simple-conan --confirm
