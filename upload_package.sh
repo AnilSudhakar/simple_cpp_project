2457490ba48f094ecec5eee31d0870e98be3e3ee
Anil Kaiyambally Sudhakar <anils.kaiyamballi@gmail.com>
2025-01-05 19:44:21 +0100

    corrected package version

--- upload_package.sh
+++ upload_package.sh
@@ -7,4 +7,4 @@ CLEAR='\033[0m'
 echo "${GREEN}Uploading Conan package...${CLEAR}"
 
 conan remote add simple-conan https://anilkaiy.jfrog.io/artifactory/api/conan/simple-conan
-conan upload simple_cpp_project/0.0.1@official/release --remote=simple-conan --confirm
+conan upload simple_cpp_project/1.0.0@official/release --remote=simple-conan --confirm
