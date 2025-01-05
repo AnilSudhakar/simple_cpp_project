9868b05d19f87b5fad5d1f5d8e141d2ce6c63da9
Anil Kaiyambally Sudhakar <58003215+AnilSudhakar@users.noreply.github.com>
2025-01-05 15:22:12 +0100

    Feature/test conan package (#7)

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,16 @@
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import copy
 
 class SimpleCPPConan(ConanFile):
     name = "simple_cpp_project"
     version = "0.0.1"
+    description = "A simple end to end C++ project"
+    url = "https://github.com/AnilSudhakar/simple_cpp_project.git"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
     default_options = {"gtest/*:shared": False}
 
     def layout(self):
@@ -13,8 +18,30 @@ class SimpleCPPConan(ConanFile):
 
     def requirements(self):
         self.requires("gtest/1.13.0")
+    
+    def generate(self):
+        cmake = CMakeToolchain(self)
+        cmake.variables["BUILD_PACKAGE"] = "ON"
+        cmake.generate()
+        return cmake
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
+    
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+    
+    def package_info(self):
+        lib_dir = os.path.join(self.package_folder, "lib")
+        include_dir = os.path.join(self.package_folder, "include")
+        libs = ["logger"]
+
+        self.cpp_info.libdirs.append(lib_dir)
+        self.cpp_info.includedirs.append(include_dir)
+        self.cpp_info.libs = libs
+    
+    def export_sources(self):
+        copy(self, "*", src=self.recipe_folder, dst=self.export_sources_folder)
---
+++ test_package/conanfile.py
@@ -0,0 +1,28 @@
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+
+
+class TestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def test(self):
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "testApp")
+            return_code = self.run(cmd, env="conanrun")
+            if return_code != 0:
+                exit(return_code)
